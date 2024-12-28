4911fdce4fdeed62fce0ccd402b416fc09eda355
dan <md.erhenede@gmail.com>
2024-12-28 20:56:13 +0100

    api updated

--- Server/src/Routes/auth.route.ts
+++ Server/src/Routes/auth.route.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
 import { login, logout, signup } from "../Helpers/auth.controller";
+import User from "../Models/User.model";
 
 const router = Router();
 
+router.post("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(201).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
